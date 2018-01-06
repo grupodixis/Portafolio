b75ea5c40bdb2bba64a65bfb9bb8bf069f03f0a7
Pablo sanchez <grupodixis@gmail.com>
2018-01-06 20:26:22 +0100

    Servicio descriptivo ajax

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,14 +5,25 @@ export class InformacionService {
 
   info: any ={};
   cargada:boolean = false;
-
+  cargo:boolean = false;
   constructor(public http:Http) {
-    this.http.get("assets/data/info.pagina.json")
-            .subscribe( data =>{
-              console.log(data.json());
-              this.cargada = true;
-              this.info = data.json();
-            })
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+public carga_info(){
+  this.http.get("assets/data/info.pagina.json")
+      .subscribe( data =>{
+          this.cargada = true;
+          this.info = data.json();
+      })
+}
+public carga_sobre_nosotros(){
+  this.http.get("https://paginaweb-f35ad.firebaseio.com/equipo.json")
+    .subscribe(data =>{
+      this.cargo = true;
+      this.about = data.json();
+      console.log(data.json());
+    })
   }
 
 }
