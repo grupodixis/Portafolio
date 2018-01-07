76fa50c5131a00edfdd5e28a7709802e56f776dd
Pablo sanchez <grupodixis@gmail.com>
2018-01-07 12:28:24 +0100

    directivas en portfolio-item html

--- src/app/components/portfolio-item/portfolio-item.component.ts
+++ src/app/components/portfolio-item/portfolio-item.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-
+import {ProductosService} from "../../services/productos.service";
 @Component({
   selector: 'app-portfolio-item',
   templateUrl: './portfolio-item.component.html'
 })
 export class PortfolioItemComponent {
-  constructor(private route:ActivatedRoute){
+  producto:any=undefined;
+  constructor(private route:ActivatedRoute, private _ps:ProductosService){
     route.params.subscribe(parametros =>{
       console.log(parametros);
       console.log(parametros['id']);
+      _ps.cargar_producto(parametros['id'])
+        .subscribe(res =>{
+          this.producto = res.json();
+        })
     })
   }
 
